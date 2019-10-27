36447dc209898e9a744ba2bcb247bbd29ceb1dfd
Tracie T <traciecodes@gmail.com>
2019-10-27 13:34:07 -0500

    Successfully pulling from the API

--- dev-ed-recipe-app/src/App.js
+++ dev-ed-recipe-app/src/App.js
@@ -3,21 +3,32 @@ import React, { useEffect, useState } from 'react';
 const App = () => {
 	const APP_ID = 'd7aadb1a';
 	const APP_KEY = '47582c17449a2bb44748510905b12ca0';
-	const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-	const [counter, setCounter] = useState(0);
-	useEffect(() => {
-		console.log('Effect has run');
-	});
+	const [recipes, setRecipes] = useState([]);
+	useEffect(
+		async () => {
+			// whenever something renders the first time, or rerenders, useEffect will run
+			getRecipes();
+		},
+		[] /* only runs when counter changes if you add counter to [] */
+	);
+
+	const getRecipes = async () => {
+		const response = await fetch(
+			`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+		);
+		const data = await response.json();
+		console.log(data.hits);
+	};
 	return (
 		<div className='App'>
 			<form className='search-form'>
 				<input type='text' className='search-bar' />
 				<button type='submit' className='search-button'>
-					Submit for real
+					Search
 				</button>
 			</form>
-			<h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+			{/* <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1> */}
 		</div>
 	);
 };
